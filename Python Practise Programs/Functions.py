885fbc5eff98c5af03ad7ae9fbd4762a43d1b907
Anmol <anmol.temp@anudip.org>
2023-10-28 13:06:35 +0530

    Create Functions.py

---
+++ Python Practise Programs/Functions.py
@@ -0,0 +1,15 @@
+# math_operations.py (module)
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+# main.py
+import math_operations
+
+result1 = math_operations.add(5, 3)
+result2 = math_operations.subtract(10, 4)
+
+print(f"Addition: {result1}")
+print(f"Subtraction: {result2}")
